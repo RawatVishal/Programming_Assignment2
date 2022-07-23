45cac1de3827700b27b0f0e6b07cc3b6e0481ff8
Vishal Rawat <vishalrwt209@gmail.com>
2022-07-23 12:18:43 -0700

    this is my programming assignment

--- Programming_assignment2.py
+++ Programming_assignment2.py
@@ -1,9 +1,21 @@
-
+# Q1-write a program to convert kilometers into miles
 
 def kms_to_miles():
+    """ This function is used to kilometers into miles"""
     a = int(input("enter the value you want to convert into miles:"))
     b = a*0.62137119
     print (a,'kms converted into ',b ,'miles')
 
 kms_to_miles()
 
+#Q2-Write a python program to convert Celsius into Fahrenheit ?
+
+def C_to_F():
+    '''This function is used to convert celsius into fahrenheit'''
+    C = int(input('enter temperature in celsius:'))
+    F = (C * (9/5)) + 32
+    print(C,"degree celsius converted into",F,"into Fahrenheit")
+
+C_to_F()
+
+#Q3-
