c9280e0c6018791011f07c9d6d4ed9c8f146c898
Vishal Rawat <vishalrwt209@gmail.com>
2022-07-23 11:58:35 -0700

    this is my programming assignment

---
+++ Programming_assignment2.py
@@ -0,0 +1,9 @@
+
+
+def kms_to_miles():
+    a = int(input("enter the value you want to convert into miles:"))
+    b = a*0.62137119
+    print (a,'kms converted into ',b ,'miles')
+
+kms_to_miles()
+
